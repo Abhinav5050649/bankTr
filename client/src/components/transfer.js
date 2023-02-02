5732449e2f4f6ca5e8c25f3377b528b5c88807ca
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-02-03 00:04:01 +0530

    Made changes to client

--- client/src/components/transfer.js
+++ client/src/components/transfer.js
@@ -3,10 +3,24 @@ import { useNavigate } from "react-router-dom";
 
 const Transfer = () => {
 
+    const [amtDefine, setAmtDefine] = React.useState(0.00)
+    const [receiverEmail, setReceiverEmail] = React.useState('');
+
+
+    const handleTransaction = () => {
     
+    }
     return(
-        <div>
-            
+        <div className="input-group">
+            <label>Amount at present: {}</label><br/>
+
+            <label>Enter amount: </label>
+            <input type="number" className="input-control" value={amtDefine} onChange={(e) => setAmtDefine(e.target.value)} id="textFormControlInput1" required={true}></input><br/>
+
+            <label>Enter receiver's EmailId: </label>
+            <input type="email" className="input-control" value={receiverEmail} onChange={(e) => setReceiverEmail(e.target.value)} id="textFormControlInput1" required={true}></input><br/>
+
+            <button type="button" class="btn btn-success" onClick={handleTransaction}>Transfer</button>
         </div>
     )
 };
