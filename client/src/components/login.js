58fbf021b55f4373db9515a3fb1b5c75cb5e686f
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-02-09 23:42:10 +0530

    changes to client components

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -5,7 +5,10 @@ export const Login = () => {
     
     let navigate = useNavigate();
     //create indivdual changing components
-    const [credentials, setCredentials] = useState({email: "", password: ""});
+    //const [credentials, setCredentials] = useState({email: "", password: ""});
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
     const handleSubmit = async(e) => {
         console.log(`Clicked`);
@@ -16,7 +19,7 @@ export const Login = () => {
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({email: credentials.email, password: credentials.password})
+            body: JSON.stringify({email: email, password: password})
         })
         
         const json = await response.json();
@@ -32,20 +35,20 @@ export const Login = () => {
         }
     }
 
-    const onChange = (e) => {
-        setCredentials({...credentials, [e.target.name]: e.target.value});
-    };
+    // const onChange = (e) => {
+    //     setCredentials({...credentials, [e.target.name]: e.target.value});
+    // };
 
     return(
         <div>
                 <form onSubmit={handleSubmit}> 
                     <div className="form-group">
                         <label>Email</label>
-                        <input type="email" className="form-control" value={credentials.email} onChange={onChange} id="textFormControlInput1" required={true}></input>
+                        <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} id="textFormControlInput1" required={true}></input>
                     </div>
                     <div className="form-group">
                         <label>Password</label>
-                        <input type="password" className="form-control" value={credentials.password} onChange={onChange} id="textFormControlInput1" required={true}></input>
+                        <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} id="textFormControlInput1" required={true}></input>
                     </div>
                 </form>
         </div>
--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -5,49 +5,59 @@ const Signup = () => {
 
     let navigate = useNavigate();
     //create individual changing components
-    const [credentials, setCredentials] = useState({name: "", email: "", password: "", secPass: ""});
-    
+    //const [credentials, setCredentials] = useState({name: "", email: "", password: "", secPass: ""});
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [secPass, setSecPass] = useState("")
+
     const handleSubmit = async(e) => {
         console.log(`Clicked`);
         e.preventDefault();
 
-        const response = await fetch(`/api/auth/createuser`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({name: credentials.name, email: credentials.email , password: credentials.password }),
-        });
+        if (password === secPass){
+            const response = await fetch(`/api/auth/createuser`, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({name: name, email: email , password: password}),
+            });
+            
+            const json = await response.json();
+            console.log(json);
+            
+            localStorage.setItem('token', json.authToken);
+            navigate("/login");
+        }
+        else{
+            alert("Please properly confirm password!");
+        }
         
-        const json = await response.json();
-        console.log(json);
-        
-        localStorage.setItem('token', json.authToken);
-        navigate("/login");
     }
 
-    const onChange = (e) => {
-        setCredentials({...credentials, [e.target.name]: e.target.value});
-    };
+    // const onChange = (e) => {
+    //     setCredentials({...credentials, [e.target.name]: e.target.value});
+    // };
 
     return (
             <div>
                 <form onSubmit={handleSubmit}> 
                     <div className="form-group">
                         <label>Name</label>
-                        <input type="text" className="form-control" value={credentials.name} onChange={onChange} id="textFormControlInput1" required={true}></input>
+                        <input type="text" className="form-control" value={name} onChange={(e)=> setName(e.target.value)} id="textFormControlInput1" required={true}></input>
                     </div>
                     <div className="form-group">
                         <label>Email</label>
-                        <input type="email" className="form-control" value={credentials.email} onChange={onChange} id="textFormControlInput1" required={true}></input>
+                        <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} id="textFormControlInput1" required={true}></input>
                     </div>
                     <div className="form-group">
                         <label>Password</label>
-                        <input type="password" className="form-control" value={credentials.password} onChange={onChange} id="textFormControlInput1" required={true}></input>
+                        <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} id="textFormControlInput1" required={true}></input>
                     </div>
                     <div className="form-group">
                         <label>Confirm Password</label>
-                        <input type="password" className="form-control" value={credentials.secPass} onChange={onChange} id="textFormControlInput1" required={true}></input>
+                        <input type="password" className="form-control" value={secPass} onChange={(e) => setSecPass(e.target.value)} id="textFormControlInput1" required={true}></input>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
