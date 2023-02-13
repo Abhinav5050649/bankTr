c39a8f9203a7f48d1c32427dceee252f6da7e22f
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-02-13 19:49:57 +0530

    Changes overall

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const mongooseURL = 'mongodb+srv://abhinavsharma:abhinav1234@cluster0.tu3yoxu.mongodb.net/testdbs?retryWrites=true&w=majority';
+const mongooseURL = '';
 
 const connectToMongo = () => {
     mongoose.connect(mongooseURL, () => {
